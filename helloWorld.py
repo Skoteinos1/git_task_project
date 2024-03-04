20f2e95e0719ad037070fef4a5c7b266506267e7
Skoteinos1 <pnagy11+gh@gmail.com>
2024-03-04 17:35:44 +0000

    User can now input data

--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,9 @@
 # Prints out: Git is awesome!
 print("Git is awesome!")
 
+# I forgot to take screenshot, so I am doing it again
+print(input('Enter text:'))
+
 # Prints out users input
 users_input = input('Enter something:')
 print(users_input)
